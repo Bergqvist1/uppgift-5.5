9d65e95053bb8cff1b413ed8e4cb0c6016b1b5fd
leonkarlsson1 <Admin@TEB12704>
2023-01-27 10:39:25 +0100

    Add project files.

---
+++ uppgift 5.5/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace Uppgift_5_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] svar = new string[10];
+
+            svar[0] = "Absolut kompis";
+            svar[1] = "Jag tycka det är bra";
+            svar[2] = "Mycket gott";
+            svar[3] = "Lägg av nu";
+            svar[4] = "Ta det lite keno grabben";
+            svar[5] = "Du behöver Jesus";
+            svar[6] = "Inte bra kombis";
+            svar[7] = "Det här är semantiskt fantastiskt";
+            svar[8] = "Det la veckan de va";
+            svar[9] = "SUUUUUI";
+
+            Random slump = new Random();
+
+            Console.WriteLine("Hur många frågor vill du ställa?");
+            int antal = int.Parse(Console.ReadLine());
+            string[] frågor = new string[antal];
+
+            for(int i = 0; i < antal; i++)
+            {
+                int val = slump.Next(svar.Length);
+                Console.WriteLine("Skriv din" + " " + (i+1) + " fråga ");
+                string fråga = Console.ReadLine();
+                Console.WriteLine("\n" + svar[val]);
+
+
+            }
+
+
+
+
+        }
+    }
+}
